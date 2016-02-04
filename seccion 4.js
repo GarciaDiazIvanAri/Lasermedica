f12d3d16083ebd3eedfa2f9c637da3e39e83721f
Ari Garcia <garciadiazivanari@gmail.com>
2016-02-04 14:05:24 -0800

    Create seccion 4.js

---
+++ seccion 4.js
@@ -0,0 +1,264 @@
++export default {
++  block1: {
++    texts: {
++      text1: 'DERMATOLOGÍA COSMETICA',
++      text2: 'DERMATOLOGÍA CLINICA',
++      text3: 'TRATAMIENTOS DERMATOLÓGICOS',
++    },
++    links: {
++        link1: {
++          href: 'servicios/dermatologia-cosmetica',
++          title: ''
++        },
++        link2: {
++          href: 'servicios/dermatologia-clinica',
++          title: ''
++        },
++        link3: {
++          href: 'servicios/tratamientos-dermatologicos',
++          title: ''
++        },
++      },
++    images: {
++      image1: {
++        src: '',
++        alt: 'imagen principal servicios',
++        },
++      },
++   },
++
++  block2: {
++    texts: {
++      text1: 'DERMATOLOGÍA COSMÉTICA',
++      text2: 'Existen diferentes tipos de procedimientos dermatológicos cosméticos a los que te puedes someter dependiendo de tu condición de la piel y del resultado que quieras obtener.',
++      text3: 'En base a las necesidades de tu cuerpo, te podemos guiar a través de diferentes procedimientos.',
++      text4: 'AGENDAR CITA',
++      text5: 'Depilación láser',
++      text6: 'Tratamiento 360',
++      text7: 'Láser Erbium Fraccionado',
++      text8: 'Láser CO2 Fraccionado',
++      text9: 'Radiofrecuencia',
++      text10: 'IPL (vasculight)',
++      text11: 'Escleroterapia con Láser',
++      text12: 'Escleroterapia Inyectada',
++      text13: 'Carboxiterapia',
++      text14: 'Liquid Face Lift',
++      text15: 'Rellenos Dérmicos',
++      text16: 'Toxina Botulínica',
++      text17: 'Mesoterapia Corporal',
++      text18: 'Mesoterapia Facial',
++      text19: 'Faciales',
++      text20: 'Microdermoabrasión',
++      text21: 'Peeling Químicos',
++      text22: 'Láser Q Switched',
++      text23: 'Plasma rico en plaquetas',
++      },
++    links: {
++      link1: {
++        href: '/contacto/agendar-cita',
++        title: ''
++        },
++      link2: {
++        href: '/servicios/depilacion-laser',
++        title: ''
++        },
++      link3: {
++        href: '/servicios/tratamiento-360',
++        title: ''
++        },
++      link4: {
++        href: '/servicios/laser-erbium-fraccionado',
++        title: ''
++        },
++      link5: {
++        href: '/servicios/laser-CO2-fraccionado',
++        title: ''
++        },
++      link6: {
++        href: '/servicios/radiofrecuencia',
++        title: ''
++        },
++      link7: {
++        href: '/servicios/IPL(vasculight)',
++        title: ''
++        },
++      link8: {
++        href: '/servicios/escleroterapia-con-laser',
++        title: ''
++        },
++      link9: {
++        href: '/servicios/escleroterapia-inyectada',
++        title: ''
++        },
++      link10: {
++        href: '/servicios/carboxiterapia',
++        title: ''
++        },
++      link11: {
++        href: '/servicios/liquid-face-lift',
++        title: ''
++        },
++      link12: {
++        href: '/servicios/rellenos-dérmicos',
++        title: ''
++        },
++      link13: {
++        href: '/servicios/toxina-botulínica',
++        title: ''
++        },
++      link14: {
++        href: '/servicios/mesoterapia-corporal',
++        title: ''
++        },
++      link15: {
++        href: '/servicios/mesoterapia-facial',
++        title: ''
++        },
++      link16: {
++        href: '/servicios/faciales',
++        title: ''
++        },
++      link17: {
++        href: '/servicios/microdermoabrasion',
++        title: ''
++        },
++      link18: {
++        href: '/servicios/peeling-quimicos',
++        title: ''
++        },
++      link19: {
++        href: '/servicios/laser-q-switched',
++        title: ''
++        },
++      link20: {
++        href: '/servicios/plasma-rico-en-plaquetas',
++        title: ''
++        },
++      },
++    images: {
++      image1: {
++        src: '',
++        alt: 'dermatologia cosmetica',
++        },
++     },
++
+++  block3: {
++    texts: {
++      text1: 'DERMATOLOGÍA CLÍNICA',
++      text2: 'En Lasermedica ofrecemos los mejores tratamientos en cuanto dermatología clínica, atendiendo enfermedades que afectan tu piel, ofreciendo su prevención, diagnóstico y tratamiento.',
++      text3: 'En base a las necesidades de tu cuerpo, te podemos guiar a través de diferentes procedimientos.',
++      text4: 'AGENDAR CITA',
++      text5: 'Acné',
++      text6: 'Cicatrices de Acné',
++      text7: 'Melasma',
++      text7: 'Psoriasis',
++      text7: 'Vitiligio',
++      text7: 'Verrugas Vulgares',
++      text7: 'Fibromas laxos',
++      text7: 'Várices',
++      text7: 'Cáncer de Piel',
++      text7: 'Alopecia',
++      text7: 'Rosácea',
++      text7: 'Hiperhidrosis',
++      text7: 'Molusco Contagioso',
++      },
++    links: {
++      link1: {
++        href: '/contacto/agendar cita,
++        title: ''
++        },
++      link1: {
++        href: '/servicios/acne',
++        title: ''
++        },
++      link2: {
++        href: '/servicios/cicatrices-de-acne',
++        title: ''
++        },
++      link3: {
++        href: '/servicios/melasma',
++        title: ''
++        },
++      link3: {
++        href: '/servicios/psoriasis',
++        title: ''
++        },
++      link3: {
++        href: '/servicios/vitiligio',
++        title: ''
++        },
++      link3: {
++        href: '/servicios/verrugas-vulgares',
++        title: ''
++        },
++      link3: {
++        href: '/servicios/fibromas-laxos',
++        title: ''
++        },
++      link3: {
++        href: '/servicios/varices',
++        title: ''
++        },
++      link3: {
++        href: '/servicios/cancer-en-la-piel',
++        title: ''
++        },
++      link3: {
++        href: '/servicios/alopecia',
++        title: ''
++        },
++      link3: {
++        href: '/servicios/rosacea',
++        title: ''
++        },
++      link3: {
++        href: '/servicios/hiperhidrosis',
++        title: ''
++        },
++      link3: {
++        href: '/servicios/molusco-contagioso',
++        title: ''
++        },
++      },
++    images: {
++      image1: {
++        src: '',
++        alt: 'dermatologia clinica',
++        },
++     },
++
++  block4: {
++    texts: {
++      text1: 'TRATAMIENTOS DERMATOLOGICOS',
++      text2: 'En Lasermedica mejoramos la apariencia de tu piel; nuestro objetivo es que nuestros pacientes luzcan una piel saludable y radiante.',
++      text3: 'En base a las necesidades de tu piel, te podemos guiar a través de diferentes procedimientos.',
++      text4: 'AGENDAR CITA',
++      text5: 'Rejuvenecimiento',
++      text6: 'Reafirmación de la Piel',
++      text7: 'Remoción: Cicatrices',
++      },
++    links: {
++      link1: {
++        href: '/contacto/agendar cita,
++        title: ''
++        },
++      link1: {
++        href: '/servicios/rejuvenecimiento',
++        title: ''
++        },
++      link2: {
++        href: '/servicios/reafirmación-de-la-piel',
++        title: ''
++        },
++      link3: {
++        href: '/servicios/remocion-cicatrices',
++        title: ''
++        },
++      },
++    images: {
++      image1: {
++        src: '',
++        alt: 'tratamientos dermatologicos',
++        },
++     },
++
