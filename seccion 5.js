15ed2804a0c817f03b3520c84e184e4dd5cb075a
Ari Garcia <garciadiazivanari@gmail.com>
2016-02-05 12:03:07 -0800

    Create seccion 5.js

---
+++ seccion 5.js
@@ -0,0 +1,272 @@
++export default {
++  block1: {
++    texts: {
++      text1: 'Nuestros Servicios',
++      text2: 'Conoce nuestros servicios y descubre cómo podemos ayudarte.',
++      text3: 'Dermatológia Cosmética',
++      text4: 'VER SERVICIOS',
++      text5: 'Depilación láser',
++      text6: 'Tratamiento 360',
++      text7: 'Láser Erbium Fraccionado',
++      text8: 'Láser CO2 Fraccionado',
++      text9: 'Radiofrecuencia',
++      text10: 'IPL (vasculight)',
++      text11: 'Terapia con Láser',
++      text12: 'Dermatológia Clínica',
++      text13: 'VER SERVICIOS',
++      text14: 'Escleroterapia Inyectada',
++      text15: 'Carboxiterapia',
++      text16: 'Liquid Face Lift',
++      text17: 'Rellenos Dérmicos',
++      text18: 'Toxina Botulínica',
++      text19: 'Mesoterapia Corporal',
++      text20: 'Mesoterapia Facial',
++      text21: 'Tratamientos Dermatológicos',
++      text22: 'VER SERVICIOS',
++      text23: 'Faciales',
++      text24: 'Microdermoabrasión',
++      text25: 'Peeling Químicos',
++      text26: 'Láser Q Switched',
++      text27: 'Plasma rico en plaquetas',
++    },
++    links: {
++        link1: {
++          href: 'servicios/ver-servicios',
++          title: ''
++        },
++        link2: {
++          href: 'servicios/depilacion-laser',
++          title: ''
++        },
++        link3: {
++          href: 'servicios/tratamiento-360',
++          title: ''
++        },
++        link4: {
++          href: 'servicios/laser-erbium-fraccionado',
++          title: ''
++        },
++        link5: {
++          href: 'servicios/laser-co2-fraccionado',
++          title: ''
++        },
++        link6: {
++          href: 'servicios/radiofrecuencia',
++          title: ''
++        },
++        link7: {
++          href: 'servicios/ipl-vasculight',
++          title: ''
++        },
++        link8: {
++          href: 'servicios/terapia-con-laser',
++          title: ''
++        },
++        link9: {
++          href: 'servicios/ver-servicios',
++          title: ''
++        },
++        link10: {
++          href: 'servicios/escleroterapia-inyectada',
++          title: ''
++        },
++        link11: {
++          href: 'servicios/carboxiterapia',
++          title: ''
++        },
++        link12: {
++          href: 'servicios/liquid-face-lift',
++          title: ''
++        },
++        link13: {
++          href: 'servicios/rellenos-dermicos',
++          title: ''
++        },
++        link14: {
++          href: 'servicios/toxina-botulinica',
++          title: ''
++        },
++        link15: {
++          href: 'servicios/mesoterapia-corporal',
++          title: ''
++        },
++        link16: {
++          href: 'servicios/mesoterapia-facial',
++          title: ''
++        },
++        link17: {
++          href: 'servicios/ver-servicios',
++          title: ''
++        },
++        link18: {
++          href: 'servicios/faciales',
++          title: ''
++        },
++        link19: {
++          href: 'servicios/microdermoabrasion',
++          title: ''
++        },
++        link20: {
++          href: 'servicios/peeling-quimicos',
++          title: ''
++        },
++        link21: {
++          href: 'servicios/laser-q-switched',
++          title: ''
++        },
++        link22: {
++          href: 'servicios/plasma-rico-en-plaquetas',
++          title: ''
++       },
++      },
++    images: {
++      image1: {
++        src: '',
++        alt: 'nuestros servicios',
++        },
++      },
++   },
++
++  block2: {
++    texts: {
++      text1: 'Nuestros Servicios',
++      text2: 'Conoce nuestros servicios y descubre cómo podemos ayudarte.',
++      text3: 'Dermatológia Cosmética',
++      text4: 'VER SERVICIOS',
++      text5: 'Dermatológia Clínica',
++      text6: 'VER SERVICIOS',
++      text7: 'Acné',
++      text8: 'Cicatrices de Acné',
++      text9: 'Melasma',
++      text10: 'Psoriasis',
++      text11: 'Vitiligio',
++      text12: 'Verrugas Vulgares',
++      text13: 'Fibromas laxos',
++      text14: 'Tratamientos Dermatológicos',
++      text15: 'VER SERVICIOS',
++      text16: 'Várices',
++      text17: 'Alopecia',
++      text18: 'Rosácea',
++      text19: 'Hiperhidrosis',
++      text20: 'Molusco Contagioso',
++    },
++    links: {
++        link1: {
++          href: 'servicios/ver-servicios',
++          title: ''
++        },
++        link2: {
++          href: 'servicios/ver-servicios',
++          title: ''
++        },
++        link3: {
++          href: 'servicios/acne',
++          title: ''
++        },
++        link4: {
++          href: 'servicios/cicatrices-de-acne',
++          title: ''
++        },
++        link5: {
++          href: 'servicios/melasma',
++          title: ''
++        },
++        link6: {
++          href: 'servicios/psoriasis',
++          title: ''
++        },
++        link7: {
++          href: 'servicios/vitiligio',
++          title: ''
++        },
++        link8: {
++          href: 'servicios/verrugas-vulgares',
++          title: ''
++        },
++        link9: {
++          href: 'servicios/fibromas-laxos',
++          title: ''
++        },
++        link10: {
++          href: 'servicios/ver-servicios',
++          title: ''
++        },
++        link11: {
++          href: 'servicios/varices',
++          title: ''
++        },
++        link12: {
++          href: 'servicios/cancer-de-piel',
++          title: ''
++        },
++        link13: {
++          href: 'servicios/alopecia',
++          title: ''
++        },
++        link14: {
++          href: 'servicios/rosacea',
++          title: ''
++        },
++        link15: {
++          href: 'servicios/hiperhidrosis',
++          title: ''
++        },
++        link16: {
++          href: 'servicios/molusco-contagioso',
++          title: ''
++        },
++      },
++    images: {
++      image1: {
++        src: '',
++        alt: 'nuestros servicios',
++        },
++      },
++   },
++  block3: {
++    texts: {
++      text1: 'Nuestros Servicios',
++      text2: 'Conoce nuestros servicios y descubre cómo podemos ayudarte.',
++      text3: 'Dermatológia Cosmética',
++      text4: 'VER SERVICIOS',
++      text5: 'Dermatológia Clínica',
++      text6: 'VER SERVICIOS',
++      text7: 'Tratamientos Dermatológicos',
++      text8: 'VER SERVICIOS',
++      text9: 'Rejuvenecimiento',
++      text10: 'Reafirmación de la Piel',
++      text11: 'Remoción: Cicatrices',
++    },
++    links: {
++        link1: {
++          href: 'servicios/ver-servicios',
++          title: ''
++        },
++        link2: {
++          href: 'servicios/ver-servicios',
++          title: ''
++        },
++        link3: {
++          href: 'servicios/ver-servicios',
++          title: ''
++        },
++        link4: {
++          href: 'servicios/rejuvenecimiento',
++          title: ''
++        },
++        link5: {
++          href: 'servicios/reafirmacion-de-la-piel',
++          title: ''
++        },
++        link6: {
++          href: 'servicios/remocion-cicatrices',
++          title: ''
++        },
++      },
++    images: {
++      image1: {
++        src: '',
++        alt: 'nuestros servicios',
++        },
++      },
++   },
