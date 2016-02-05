e63b6abca78b4438dc924096ad626c7e883cc471
Ari Garcia <garciadiazivanari@gmail.com>
2016-02-05 12:44:23 -0800

    Create seccion 6.js

---
+++ seccion 6.js
@@ -0,0 +1,163 @@
++export default {
++  block1: {
++    texts: {
++      text1: 'Dermatología Cosmética',
++      text2: 'Menú de Servicios',
++      text3: 'Anterior',
++      text4: 'Siguiente',
++    },
++    links: {
++        link1: {
++          href: 'servicios/dermatologia-cosmetica-izquierda',
++          title: ''
++        },
++        link2: {
++          href: 'servicios/dermatologia-cosmetica-derecha',
++          title: ''
++        },
++        link3: {
++          href: 'servicios/menu-de-servicios',
++          title: ''
++        },
++        link4: {
++          href: '/anterior',
++          title: ''
++        },
++        link5: {
++          href: '/siguiente',
++          title: ''
++        },
++       },
++    images: {
++      image1: {
++        src: '',
++        alt: 'dermatologia cosmetica',
++        },
++      },
++    },
++
++  block2: {
++    texts: {
++      text1: 'Depilación láser',
++      text2: 'Tratamiento 360',
++      text3: 'Láser Erbium Fraccionado',
++      text4: 'Láser CO2 Fraccionado',
++      text5: 'Radiofrecuencia',
++      text6: 'IPL (vasculight)',
++      text7: 'Escleroterapia con Láser',
++      text8: 'Escleroterapia Inyectada',
++      text9: 'Carboxiterapia',
++      text10: 'Liquid Face Lift',
++      text11: 'Rellenos Dérmicos',
++      text12: 'Toxina Botulínica',
++      text13: 'Mesoterapia Corporal',
++      text14: 'Mesoterapia Facial',
++      text15: 'Faciales',
++      text16: 'Microdermoabrasión',
++      text17: 'Peeling Químicos',
++      text18: 'Láser Q Switched',
++      text19: 'Plasma rico en plaquetas',
++      text20: 'Tratamiento 360',
++      text20: '¿QUÉ ES?',
++      text20: 'En láser médica sabemos de la importancia de un cutis terso y fresco y es por ello que te ofrecemos el tratamiento 360, un método rápido y lo más importante no invasivo que da a tu piel un aspecto rejuvenecido y radiante con un aspecto natural. Este tratamiento combate arrugas finas, manchas faciales, mejora el tono de la piel y ayuda a eliminar la flacidez, dándole firmeza al rosto y cuello a través de una combinación de técnicas como el láser fraccionado, IPL y radio frecuencia según las necesidades de cada paciente mostrando resultados a tan sólo 60 días de haber iniciado el tratamiento.',
++      text20: 'OBJETIVOS',
++      text20: 'Rejuvenecimiento de la piel.',
++      text20: 'Mejorar color.',
++      text20: 'Apariencia más lozana y textura más suave.',
++      text20: '¿QUÉ TÉCNICAS UTILIZAMOS?',
++      text20: 'La triple combinación de láser fraccionado, IPL y haz de luz mejorarán invariablemente el aspecto de tu piel. El láser fraccionado ayuda a mejorar textura y tono, también a borrar arrugas pequeñas, aunado a lo anterior también cierra poros y alisa la piel, Lo mejor de todo es que este tratamiento no causa molesta alguna. El láser IPL (por sus siglas en inglés Luz Pulzada Intensa) elimina manchas en la piel mejorando visiblemente su aspecto pues el haz de luz que emite destruye las lesiones que provocan dichas manchas. Y por último, la terapia de radio frecuencia ayuda a dar firmeza a la piel, induce a la producción de colágeno mejorando también los contornos del rostro. Te invitamos a hacer una cita con nosotros para ayudarte a elegir el tratamiento específico para tu piel, en Láser médica nos preocupamos por tener tecnología de punta y ofrecer el mejor servicio a nuestros clientes.',
++      text20: 'AGEN DAR CITA',
++    },
++    links: {
++        link1: {
++          href: 'servicios/depilacion-laser',
++          title: ''
++        },
++        link2: {
++          href: 'servicios/tratamiento-360',
++          title: ''
++        },
++        link3: {
++          href: 'servicios/laser-erbium-fraccionado',
++          title: ''
++        },
++        link4: {
++          href: 'servicios/laser-co2-fraccionado',
++          title: ''
++        },
++        link5: {
++          href: 'servicios/radiofrecuencia',
++          title: ''
++        },
++        link6: {
++          href: 'servicios/ipl-(vasculight)',
++          title: ''
++        },
++        link7: {
++          href: 'servicios/escleroterapia-con-laser',
++          title: ''
++        },
++        link8: {
++          href: 'servicios/escleroterapia-inyectada',
++          title: ''
++        },
++        link9: {
++          href: 'servicios/carboxiterapia',
++          title: ''
++        },
++        link10: {
++          href: 'servicios/liquid-face-lift',
++          title: ''
++        },
++        link11: {
++          href: 'servicios/rellenos-dermicos',
++          title: ''
++        },
++        link12: {
++          href: 'servicios/toxina-botulinica',
++          title: ''
++        },
++        link13: {
++          href: 'servicios/mesoterapia-corporal',
++          title: ''
++        },
++        link14: {
++          href: 'servicios/mesoterapia-facial',
++          title: ''
++        },
++        link15: {
++          href: 'servicios/faciales',
++          title: ''
++        },
++        link16: {
++          href: 'servicios/microdermoabrasion',
++          title: ''
++        },
++        link17: {
++          href: 'servicios/peeling-quimicos',
++          title: ''
++        },
++        link18: {
++          href: 'servicios/laser-q-switched',
++          title: ''
++        },
++        link19: {
++          href: 'servicios/plasma-rico-en-plaquetas',
++          title: ''
++        },
++        link20: {
++          href: 'servicios/agendar-cita',
++          title: ''
++        },
++      },
++    images: {
++      image1: {
++        src: '',
++        alt: 'dermatologia cosmetica',
++        },
++      image2: {
++        src: '',
++        alt: 'tratamiento 360',
++        },
++      },
++   },
