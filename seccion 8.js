122e0dcb57f768c4e40f94423df38e15f553a546
Ari Garcia <garciadiazivanari@gmail.com>
2016-02-05 13:39:36 -0800

    Create seccion 8.js

---
+++ seccion 8.js
@@ -0,0 +1,77 @@
++export default {
++  block1: {
++    texts: {
++      text1: 'Tratamientos Dermatológicos',
++      text2: 'Menú de Servicios',
++      text3: 'Anterior',
++      text4: 'Siguiente',
++    },
++    links: {
++        link1: {
++          href: 'servicios/tratamientos-dermatologicos-izquierda',
++          title: ''
++        },
++        link2: {
++          href: 'servicios/tratamientos-dermatologicos-derecha',
++          title: ''
++        },
++        link3: {
++          href: 'servicios/menu-de-servicios',
++          title: ''
++        },
++        link4: {
++          href: '/anterior',
++          title: ''
++        },
++        link5: {
++          href: '/siguiente',
++          title: ''
++        },
++       },
++    images: {
++      image1: {
++        src: '',
++        alt: 'tratamientos dermatologicos',
++        },
++      },
++    },
++
++  block2: {
++    texts: {
++      text1: 'Rejuvenecimiento',
++      text2: 'Reafirmación de la Piel',
++      text3: 'Remoción: Cicatrices',
++      text4: 'Rejuvenecimiento',
++      text5: 'Tener un rostro de aspecto saludable, joven y fresco son unos de los objetivos de los tratamientos que ofrecemos en láser médica; pues con la mejor tecnología buscamos suavizar la apariencia de la piel, mejorar su textura y ayudar a atenuar líneas de expresión en rostro y cuello. Dependiendo de las necesidades de cada paciente, le ayudamos a elegir los tratamientos más convenientes para lograr combatir el paso del tiempo.',
++      text6: 'AGENDAR CITA',
++      },
++    },
++    links: {
++        link1: {
++          href: 'servicios/rejuvenecimiento',
++          title: ''
++        },
++        link2: {
++          href: 'servicios/reafirmacion-de-la-piel',
++          title: ''
++        },
++        link3: {
++          href: 'servicios/remocion-cicatrices',
++          title: ''
++        },
++        link14: {
++          href: 'servicios/agendar-cita',
++          title: ''
++        },
++      },
++    images: {
++      image1: {
++        src: '',
++        alt: 'tratamientos dermatologicos',
++        },
++      image2: {
++        src: '',
++        alt: 'rejuvenecimiento',
++        },
++      },
++   },
