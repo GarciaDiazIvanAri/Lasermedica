04fb474a31f316b4b63ce1b376a260d28081bb48
Ari Garcia <garciadiazivanari@gmail.com>
2016-02-05 13:09:30 -0800

    Create seccion 7.js

---
+++ seccion 7.js
@@ -0,0 +1,133 @@
++export default {
++  block1: {
++    texts: {
++      text1: 'Dermatología Clínica',
++      text2: 'Menú de Servicios',
++      text3: 'Anterior',
++      text4: 'Siguiente',
++    },
++    links: {
++        link1: {
++          href: 'servicios/dermatologia-cosmetica-izquierda',
++          title: ''
++        },
++        link2: {
++          href: 'servicios/dermatologia-cosmetica-derecha',
++          title: ''
++        },
++        link3: {
++          href: 'servicios/menu-de-servicios',
++          title: ''
++        },
++        link4: {
++          href: '/anterior',
++          title: ''
++        },
++        link5: {
++          href: '/siguiente',
++          title: ''
++        },
++       },
++    images: {
++      image1: {
++        src: '',
++        alt: 'dermatologia clinica',
++        },
++      },
++    },
++
++  block2: {
++    texts: {
++      text1: 'Acné',
++      text2: 'Cicatrices de Acné',
++      text3: 'Melasma',
++      text4: 'Psoriasis',
++      text5: 'Vitiligio',
++      text6: 'Verrugas Vulgares',
++      text7: 'Fibromas laxos',
++      text8: 'Várices',
++      text9: 'Cáncer de Piel',
++      text10: 'Alopecia',
++      text11: 'Rosácea',
++      text12: 'Hiperhidrosis',
++      text13: 'Molusco Contagioso',
++      text13: 'Acné',
++      text13: '¿QUÉ ES?',
++      text13: 'En el mundo, tres de cada cuatro adolescentes han tenido algún tipo de acné; esta recurrencia es debido a que los cambios hormonales sufridos durante la adolescencia estimulan las glándulas sebáceas produciendo más sebo, incrementando las probabilidades de sufrir acné. Aún cuando es más común desarrollar acné al final de la adolescencia y principios de los veintes, existen casos en los que se dá en la edad adulta, suele suceder por el período menstrual, uso de pastillas anticonceptivas, productos con base de aceite y estrés.',
++      text13: '¿CÓMO LO TRATAMOS?',
++      text13: 'El medicamento que se puede utilizar puede ser tópico (aplicado en la piel) o sistémico (tomado, pastillas), a algunos pacientes se les dá un tratamiento conjunto que combina ambos procesos. El acné también se puede tratar mediante tecnología basada en láser y fototecnología.',
++      text13: 'Es el cuidado por excelencia para tratar el acné leve; algunos medicamentos tópicos comunes que los dermatólogos recomiendan incluyen peróxido de benzoilo, antibióticos, retinoides, y ácido salicílico. Los medicamentos tópicos varían en su presentación, puede ser gel, lociones y cremas. Su dermatólogo determinará el tratamiento más apropiado para su piel.',
++      text13: 'Tratamiento Sistémico',
++      text13: 'Los antibióticos orales son básicos en el cuidado del acné moderado a severo, sobre todo al que resiste el tratamiento tópico y el acné que cubre largas áreas del cuerpo. Algunos de los antibióticos orales que pueden ser prescritos para tratar el acné incluyen tetraciclina, doxiciclina, eritromicina, azitromicina, minociclina y clindamicina.',
++      text13: 'AGENDAR CITA',
++      },
++    },
++    links: {
++        link1: {
++          href: 'servicios/acne',
++          title: ''
++        },
++        link2: {
++          href: 'servicios/cicatrices-de-acne',
++          title: ''
++        },
++        link3: {
++          href: 'servicios/melasma',
++          title: ''
++        },
++        link4: {
++          href: 'servicios/psoriasis',
++          title: ''
++        },
++        link5: {
++          href: 'servicios/vitiligio',
++          title: ''
++        },
++        link6: {
++          href: 'servicios/verrugas-vulgares',
++          title: ''
++        },
++        link7: {
++          href: 'servicios/fibromas-laxos',
++          title: ''
++        },
++        link8: {
++          href: 'servicios/varices',
++          title: ''
++        },
++        link9: {
++          href: 'servicios/cancer-de-piel',
++          title: ''
++        },
++        link10: {
++          href: 'servicios/alopecia',
++          title: ''
++        },
++        link11: {
++          href: 'servicios/rosacea',
++          title: ''
++        },
++        link12: {
++          href: 'servicios/hiperhidrosis',
++          title: ''
++        },
++        link13: {
++          href: 'servicios/molusco-contagioso',
++          title: ''
++        },
++        link14: {
++          href: 'servicios/agendar-cita',
++          title: ''
++        },
++      },
++    images: {
++      image1: {
++        src: '',
++        alt: 'dermatologia clinica',
++        },
++      image2: {
++        src: '',
++        alt: 'acne',
++        },
++      },
++   },
